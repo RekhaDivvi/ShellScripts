7686aa8da7f22f09fbd4458bb132b5172d83ee3b
RekhaDivvi <43535914+RekhaDivvi@users.noreply.github.com>
2020-11-22 14:52:06 +0530

    Added shell script to reverse a number

---
+++ Coding Exercise 1/Number_Reversal.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#Storing command line paramenters in an array
+arr=$@
+
+#Validating if the script is executed with command line parameters or not
+if [ $# -ne 0 ]
+then
+#Using for loop to print reversal of more than one number
+        for n in ${arr[@]}
+		do
+                echo $n | rev
+        done
+else
+        echo "Error! Please provide command line arguments"
+fi
+
+
+
+#Alternate method using while loop
+: '
+arr=$@
+if [ $# -ne 0 ]
+then
+        for n in ${arr[@]}; do
+                REM=0
+                REV_NUM=0
+                NUMBER=$n
+                while [ $NUMBER -gt 0 ]
+                do
+                        REM=`expr $NUMBER % 10`
+                        REV_NUM=`expr $REV_NUM \* 10 + $REM`
+                        NUMBER=`expr $NUMBER / 10`
+                done
+                echo "Reverse of $n is: " $REV_NUM
+        done
+else
+        echo "Error! Please provide command line arguments"
+fi
+'
