ab5b0dfa1b68c442d1510924c3085e1c83167da5
RekhaDivvi <43535914+RekhaDivvi@users.noreply.github.com>
2020-11-23 11:43:46 +0530

    Update String_Reversal.sh

--- Coding Exercise 1/String_Reversal.sh
+++ Coding Exercise 1/String_Reversal.sh
@@ -2,5 +2,18 @@
 
 echo "Enter string: "
 read string
+len=${#string}
+while [ $len -ne 0 ]
+do
+	reverse=$reverse`echo $string | cut -c $len`
+	((len--))
+done
+echo $reverse
+
+#Using rev command
+: '
+echo "Enter string: "
+read string
 #Using rev command to reverse lines and further reverse characters in each string
 echo $string | rev
+'
